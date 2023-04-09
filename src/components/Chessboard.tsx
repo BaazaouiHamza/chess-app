d251b923847ea20047a8d5b24ec60adc55f85c11
BaazaouiHamza <hamza.baazaoui@esprit.tn>
2023-04-09 22:41:27 +0100

    creating the tiles

--- src/components/Chessboard.tsx
+++ src/components/Chessboard.tsx
@@ -2,11 +2,22 @@ import React from "react";
 
 import "./Chessboard.css";
 
-const verticalAxis = [1, 2, 3, 4, 5, 6, 7, 8];
+const verticalAxis = ["1", "2", "3", "4", "5", "6", "7", "8"];
 const horizontalAxis = ["a", "b", "c", "d", "e", "f", "g", "h"];
 
 const ChessBoard = () => {
-  return <div id="chessboard"></div>;
+  let board = [];
+
+  for (let j = verticalAxis.length - 1; j >= 0; j--) {
+    for (let i = 0; i < horizontalAxis.length; i++) {
+      const number = j + i;
+      board.push(
+        <div className={`tile ${number % 2 === 0 ? "black-tile" : "white-tile"}`}>
+        </div>
+      );
+    }
+  }
+  return <div id="chessboard">{board}</div>;
 };
 
 export default ChessBoard;
